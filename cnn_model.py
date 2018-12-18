7058c3b8e693bca36052509311439023118631ef
Lars Schürmann <lschuermann@assystem.com>
2018-12-18 14:07:05 +0100

    Changed model to original nvidia architecture

--- cnn_model.py
+++ cnn_model.py
@@ -1,6 +1,7 @@
 from keras import Sequential
 from keras.layers import Conv2D, Dropout, Flatten, Dense
 from keras.layers import Lambda
+from keras.regularizers import l2
 
 input_width = 200
 input_height = 66
@@ -16,18 +17,18 @@ def get_model():
 
     model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=input_size))
 
-    model.add(Conv2D(24, kernel_size=5, activation='relu', strides=(2, 2), kernel_initializer=init))
-    model.add(Conv2D(36, kernel_size=5, activation='relu', strides=(2, 2), kernel_initializer=init))
-    model.add(Conv2D(48, kernel_size=5, activation='relu', strides=(2, 2), kernel_initializer=init))
-    model.add(Conv2D(64, kernel_size=3, activation='relu', strides=(1, 1), kernel_initializer=init))
-    model.add(Conv2D(64, kernel_size=3, activation='relu', strides=(1, 1), kernel_initializer=init))
+    model.add(Conv2D(24, kernel_size=5, activation='relu', strides=(2, 2), kernel_initializer=init, kernel_regularizer=l2(0.001)))
+    model.add(Conv2D(36, kernel_size=5, activation='relu', strides=(2, 2), kernel_initializer=init, kernel_regularizer=l2(0.001)))
+    model.add(Conv2D(48, kernel_size=5, activation='relu', strides=(2, 2), kernel_initializer=init, kernel_regularizer=l2(0.001)))
+    model.add(Conv2D(64, kernel_size=3, activation='relu', strides=(1, 1), kernel_initializer=init, kernel_regularizer=l2(0.001)))
+    model.add(Conv2D(64, kernel_size=3, activation='relu', strides=(1, 1), kernel_initializer=init, kernel_regularizer=l2(0.001)))
     model.add(Flatten())
-    model.add(Dense(units=100))
+    model.add(Dense(units=1164, kernel_regularizer=l2(0.001)))
     model.add(Dropout(rate=dense_keep_prob))
-    model.add(Dense(units=50))
-    model.add(Dropout(rate=dense_keep_prob))
-    model.add(Dense(units=10))
-    model.add(Dropout(rate=dense_keep_prob))
+
+    model.add(Dense(units=100, kernel_regularizer=l2(0.001)))
+    model.add(Dense(units=50, kernel_regularizer=l2(0.001)))
+    model.add(Dense(units=10, kernel_regularizer=l2(0.001)))
     model.add(Dense(units=1))
 
     return model
